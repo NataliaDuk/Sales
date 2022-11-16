7ff484c41524456fc1a0232e72c6dbcdd22dac0d
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2022-11-16 13:02:07 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 return [
     "servername" => "localhost",
     "username" => "root",
-    "password" => "root",
+    "password" => "",
     "dbname" => "sales",
     "page_size" => 8,
     "salt"=>"kjsksdjt5859fdjkdfdfj"
